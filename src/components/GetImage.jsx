f7f750dd34d30cca29e3a2741295d3ede465b987
Waco <waco@Wacos-MacBook-Pro.local>
2023-01-23 21:34:48 +1000

    revert changes

--- src/components/GetImage.jsx
+++ src/components/GetImage.jsx
@@ -2,8 +2,18 @@ import React from 'react';
 
 export default function GetImage() {
   return (
-    <div className='w-[477px] h-[268px] pt-10'>
-      <img src='memeimg.png' alt='meme' />
+    <div className='pt-5'>
+      <button
+        className='w-[477px] h-[40px] text-sm bg-[#672280] 
+        rounded-md text-white font-sans antialiased 
+        bg-gradient-to-l from-[#A818DA]
+        hover:opacity-80'
+      >
+        Get a new meme Image 🖼
+      </button>
+      <div className='w-[477px] h-[268px] pt-10'>
+        <img src='./memeimg.png' alt='meme' />
+      </div>
     </div>
   );
 }
